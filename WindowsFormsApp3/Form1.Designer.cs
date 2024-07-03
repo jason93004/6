e32741f02355193335e82d63995cd15bcf4842ec
jason93004 <164745207+jason93004@users.noreply.github.com>
2024-07-03 23:39:33 +0800

    Add files via upload

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿using System;
+
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.rtbText = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listUndo = new System.Windows.Forms.ListBox();
+            this.btnUndo = new System.Windows.Forms.Button();
+            this.btnRedo = new System.Windows.Forms.Button();
+            this.comboBoxFont = new System.Windows.Forms.ComboBox();
+            this.comboBoxSize = new System.Windows.Forms.ComboBox();
+            this.comboBoxStyle = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(584, 42);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(153, 33);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "存檔";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(584, 3);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(153, 33);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "開啟檔案";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // rtbText
+            // 
+            this.rtbText.Location = new System.Drawing.Point(12, 35);
+            this.rtbText.Name = "rtbText";
+            this.rtbText.Size = new System.Drawing.Size(566, 414);
+            this.rtbText.TabIndex = 2;
+            this.rtbText.Text = "";
+            this.rtbText.TextChanged += new System.EventHandler(this.rtbText_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listUndo
+            // 
+            this.listUndo.FormattingEnabled = true;
+            this.listUndo.ItemHeight = 18;
+            this.listUndo.Location = new System.Drawing.Point(584, 157);
+            this.listUndo.Name = "listUndo";
+            this.listUndo.Size = new System.Drawing.Size(153, 292);
+            this.listUndo.TabIndex = 3;
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.Location = new System.Drawing.Point(584, 81);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(153, 33);
+            this.btnUndo.TabIndex = 4;
+            this.btnUndo.Text = "復原上一步";
+            this.btnUndo.UseVisualStyleBackColor = true;
+            this.btnUndo.TextChanged += new System.EventHandler(this.btnUndo_TextChanged);
+            // 
+            // btnRedo
+            // 
+            this.btnRedo.Location = new System.Drawing.Point(584, 120);
+            this.btnRedo.Name = "btnRedo";
+            this.btnRedo.Size = new System.Drawing.Size(153, 33);
+            this.btnRedo.TabIndex = 5;
+            this.btnRedo.Text = "重作下一步";
+            this.btnRedo.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxFont
+            // 
+            this.comboBoxFont.FormattingEnabled = true;
+            this.comboBoxFont.Location = new System.Drawing.Point(12, 3);
+            this.comboBoxFont.Name = "comboBoxFont";
+            this.comboBoxFont.Size = new System.Drawing.Size(121, 26);
+            this.comboBoxFont.TabIndex = 6;
+            this.comboBoxFont.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBoxSize
+            // 
+            this.comboBoxSize.FormattingEnabled = true;
+            this.comboBoxSize.Location = new System.Drawing.Point(148, 3);
+            this.comboBoxSize.Name = "comboBoxSize";
+            this.comboBoxSize.Size = new System.Drawing.Size(121, 26);
+            this.comboBoxSize.TabIndex = 7;
+            this.comboBoxSize.SelectedIndexChanged += new System.EventHandler(this.comboBoxSize_SelectedIndexChanged);
+            // 
+            // comboBoxStyle
+            // 
+            this.comboBoxStyle.FormattingEnabled = true;
+            this.comboBoxStyle.Location = new System.Drawing.Point(287, 3);
+            this.comboBoxStyle.Name = "comboBoxStyle";
+            this.comboBoxStyle.Size = new System.Drawing.Size(121, 26);
+            this.comboBoxStyle.TabIndex = 8;
+            this.comboBoxStyle.SelectedIndexChanged += new System.EventHandler(this.comboBoxStyle_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBoxStyle);
+            this.Controls.Add(this.comboBoxSize);
+            this.Controls.Add(this.comboBoxFont);
+            this.Controls.Add(this.btnRedo);
+            this.Controls.Add(this.btnUndo);
+            this.Controls.Add(this.listUndo);
+            this.Controls.Add(this.rtbText);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        private void btnUndo_TextChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.RichTextBox rtbText;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listUndo;
+        private System.Windows.Forms.Button btnUndo;
+        private System.Windows.Forms.Button btnRedo;
+        private System.Windows.Forms.ComboBox comboBoxFont;
+        private System.Windows.Forms.ComboBox comboBoxSize;
+        private System.Windows.Forms.ComboBox comboBoxStyle;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeFontComboBox();
+            InitializeFontSizeComboBox();
+            InitializeFontStyleComboBox();
+        }
+        private Stack<MemoryStream> undoStack = new Stack<MemoryStream>();
+        private Stack<MemoryStream> redoStack = new Stack<MemoryStream>();
+        private const int MaxHistoryCount = 10;
+        private bool isUndoRedo = false;
+        //private Stack<string> UndoStack = new Stack<string>();
+        //private Stack<string> redoStack = new Stack<string>();
+        private int selectionStart = 0;
+        private int selectionLength = 0;
+
+        private void InitializeFontComboBox()
+        {
+            foreach (FontFamily font in FontFamily.Families)
+            {
+                comboBoxFont.Items.Add(font.Name);
+            }
+            comboBoxFont.SelectedIndex = 0;
+        }
+        private void InitializeFontSizeComboBox()
+        {
+            for (int i = 8; i <= 72; i += 2)
+            {
+                comboBoxSize.Items.Add(i);
+            }
+            comboBoxSize.SelectedIndex = 2;
+        }
+        private void InitializeFontStyleComboBox()
+        {
+            comboBoxStyle.Items.Add(FontStyle.Regular.ToString());
+            comboBoxStyle.Items.Add(FontStyle.Bold.ToString());
+            comboBoxStyle.Items.Add(FontStyle.Italic.ToString());
+            comboBoxStyle.Items.Add(FontStyle.Underline.ToString());
+            comboBoxStyle.Items.Add(FontStyle.Strikeout.ToString());
+            comboBoxStyle.SelectedIndex = 0;
+        }
+
+        private void comboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectionStart = rtbText.SelectionStart;
+            selectionLength = rtbText.SelectionLength;
+            if (rtbText.SelectionFont != null)
+            {
+                string selectedFont = comboBoxFont.SelectedItem?.ToString();
+                string selectedSizeStr = comboBoxSize.SelectedItem?.ToString();
+                string selectedStyleStr = comboBoxStyle.SelectedItem?.ToString();
+                if (selectedFont != null && selectedSizeStr != null && selectedStyleStr != null)
+                {
+                    float selectedSize = float.Parse(selectedSizeStr);
+                    FontStyle selectedStyle = (FontStyle)Enum.Parse(typeof(FontStyle), selectedStyleStr);
+                    Font currentFont = rtbText.SelectionFont;
+                    FontStyle newStyle = currentFont.Style;
+                    if (comboBoxStyle.SelectedItem.ToString() == FontStyle.Bold.ToString())
+                        newStyle = FontStyle.Bold;
+                    else if (comboBoxStyle.SelectedItem.ToString() == FontStyle.Italic.ToString())
+                        newStyle = FontStyle.Italic;
+                    else if (comboBoxStyle.SelectedItem.ToString() == FontStyle.Underline.ToString())
+                        newStyle = FontStyle.Underline;
+                    else if (comboBoxStyle.SelectedItem.ToString() == FontStyle.Strikeout.ToString())
+                        newStyle = FontStyle.Strikeout;
+                    else
+                        newStyle = FontStyle.Regular;
+                    Font newFont = new Font(selectedFont, selectedSize, newStyle);
+                    rtbText.SelectionFont = newFont;
+                }
+            }
+            rtbText.Focus();
+            rtbText.Select(selectionStart, selectionLength);
+        }
+
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+            saveFileDialog1.Title = "儲存檔案";
+
+            saveFileDialog1.Filter = "RTF格式檔案 (*.rtf)|*.rtf|文字檔案 (*.txt)|*.txt|所有檔案 (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 1;
+
+            saveFileDialog1.InitialDirectory = "C:\\";
+
+
+            DialogResult result = saveFileDialog1.ShowDialog();
+
+
+            FileStream fileStream = null;
+
+
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+
+                    string saveFileName = saveFileDialog1.FileName;
+                    string extension = Path.GetExtension(saveFileName);
+                    using (fileStream = new FileStream(saveFileName, FileMode.Create, FileAccess.Write))
+                    {
+                        if (extension.ToLower() == ".txt")
+                        {
+                            byte[] data = Encoding.UTF8.GetBytes(rtbText.Text);
+                            fileStream.Write(data, 0, data.Length);
+                        }
+
+                        if (extension.ToLower() == ".txt")
+                        {
+                            byte[] data = Encoding.UTF8.GetBytes(rtbText.Text);
+                            fileStream.Write(data, 0, data.Length);
+                        }
+                        else if (extension.ToLower() == ".rtf")
+                        {
+                            rtbText.SaveFile(fileStream, RichTextBoxStreamType.RichText);
+                        }
+                    }
+                    MessageBox.Show("檔案儲存成功。", "訊息", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("儲存檔案時發生錯誤: " + ex.Message, "錯誤訊息", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    fileStream.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("使用者取消了儲存檔案操作。", "訊息", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+
+            openFileDialog1.Title = "選擇檔案";
+
+            openFileDialog1.Filter = "RTF格式檔案 (*.rtf)|*.rtf|文字檔案 (*.txt)|*.txt|所有檔案 (*.*)|*.*"; ;
+
+            openFileDialog1.FilterIndex = 1;
+
+            openFileDialog1.InitialDirectory = "C:\\";
+
+            openFileDialog1.Multiselect = true;
+
+
+            DialogResult result = openFileDialog1.ShowDialog();
+
+
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+
+                    string selectedFileName = openFileDialog1.FileName;
+
+                    string fileExtension = Path.GetExtension(selectedFileName).ToLower();
+                    if (fileExtension == ".txt")
+                    {
+                        using (FileStream fileStream = new FileStream(selectedFileName, FileMode.Open, FileAccess.Read))
+                        {
+                            using (StreamReader streamReader = new StreamReader(fileStream, Encoding.UTF8))
+                            {
+                                rtbText.Text = streamReader.ReadToEnd();
+                            }
+                        }
+                    }
+                    else if (fileExtension == ".rtf")
+                    {
+                        rtbText.LoadFile(selectedFileName, RichTextBoxStreamType.RichText);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("讀取檔案時發生錯誤: " + ex.Message, "錯誤訊息", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("使用者取消了選擇檔案操作。", "訊息", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation);
+            }
+        }
+
+
+
+
+        private void rtbText_TextChanged(object sender, EventArgs e)
+        {
+            if (isUndoRedo == false)
+            {
+                SaveCurrentStateToStack();
+                redoStack.Clear();
+                if (undoStack.Count > MaxHistoryCount)
+                {
+                    Stack<MemoryStream> tempStack = new Stack<MemoryStream>();
+                    for (int i = 0; i < MaxHistoryCount; i++)
+                    {
+                        tempStack.Push(undoStack.Pop());
+                    }
+                    undoStack.Clear();
+                    foreach (MemoryStream item in tempStack)
+                    {
+                        undoStack.Push(item);
+                    }
+                }
+                UpdateListBox();
+
+            }
+
+        }
+        private void UpdateListBox()
+        {
+            listUndo.Items.Clear();
+            foreach(MemoryStream item in listUndo.Items) 
+            {
+                listUndo.Items.Add(item);
+            }
+        }
+
+
+
+
+
+        private void btnRedo_Click(object sender, EventArgs e)
+            
+        {
+            if (redoStack.Count > 0)
+        {
+            isUndoRedo = true;
+            undoStack.Push(redoStack.Pop());
+            MemoryStream lastSavedState = undoStack.Peek();
+            LoadFromMemory(lastSavedState);
+            UpdateListBox();
+            isUndoRedo = false;
+        }
+        }
+
+        private void btnUndo_Click(object sender, EventArgs e)
+        {
+        if (undoStack.Count > 1)
+        {
+            isUndoRedo = true;
+            redoStack.Push(undoStack.Pop());
+            MemoryStream lastSavedState = undoStack.Peek();
+            LoadFromMemory(lastSavedState);
+            UpdateListBox();
+            isUndoRedo = false;
+        }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxSize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxStyle_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void SaveCurrentStateToStack()
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            rtbText.SaveFile(memoryStream, RichTextBoxStreamType.RichText);
+            undoStack.Push(memoryStream);
+        }
+        private void LoadFromMemory(MemoryStream memoryStream)
+        {
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            rtbText.LoadFile(memoryStream, RichTextBoxStreamType.RichText);
+        }
+    }
+    }
